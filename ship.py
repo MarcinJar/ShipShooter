55a975887135c723c8182f4635a0f1951178cc35
Marcin Jarmułowski <m.jarmulowski@gmail.com>
2024-09-01 20:03:44 +0200

    Add Ship class and display on the screen

---
+++ ship.py
@@ -0,0 +1,29 @@
+from __future__ import annotations 
+import pygame
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ship_shooter import ShipShooter
+
+class Ship:
+    def __init__(self, ss_game: ShipShooter) -> None:
+        """
+        Initialize the ship and set its starting position.
+        Args:
+            ss_game (ShipShooter): An instance of the ShipShooter game.
+        """
+        self.screen = ss_game.screen
+        self.screen_rect = ss_game.screen.get_rect()
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self) -> None:
+        """
+        Draw the ship at its current location.
+        This method uses the `blit` function from the pygame library to draw the ship's image onto the screen.
+        The ship's image is drawn at the position specified by the ship's rectangle.
+        """
+        self.screen.blit(self.image, self.rect)
+
+        
--- ship_shooter.py
+++ ship_shooter.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class ShipShooter:
     """
@@ -17,24 +18,32 @@ class ShipShooter:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        
+
         width = self.settings.screen_width
         height = self.settings.screen_height
         self.screen = pygame.display.set_mode((width, height))
         
+        self.ship = Ship(self)
+
     def run_game(self) -> None:
+        """
+        Run the main loop for the game.
+        This function contains an infinite loop that runs the game. It checks for events, updates the screen, and limits the frame rate.
+        """
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     sys.exit()
-            
+
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
             self.clock.tick(60)
-    
+
 if __name__ == '__main__':
     game = ShipShooter()
     game.run_game()
+
           
 
       
